0f1f29375eab7e26e35856494d645a94ad8cac5a
Peter Suggitt <me@suggs.org.uk>
2023-11-04 16:02:08 +0000

    added to

--- amazontracker/tracker.py
+++ amazontracker/tracker.py
@@ -1,18 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.amazon.co.uk/dp/B093T7F1YP'
 HEADERS = {"User-Agent":'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'}
-PRICE = 160
 
-def getPrice():
-    page = requests.get(URL, headers=HEADERS)
+
+
+def getPrices():
+    with open('trackurls.txt') as f:
+        lines = [line.rstrip('\n') for line in f]
+
+    for url in lines:
+        getPrice(url)
+
+def getPrice(url):
+    page = requests.get(url, headers=HEADERS)
     soup = BeautifulSoup(page.content, 'html.parser')
-    title = soup.find(id='productTitle')
-    price = soup.find('a-price-whole')
+    title = soup.find(id='productTitle').get_text().strip()
+    price = soup.find('span', class_ = 'a-offscreen').get_text()
     print(title)
     print(price)
-    print(page.content)
 
 if __name__ == "__main__":
-    getPrice()
+    getPrices()
