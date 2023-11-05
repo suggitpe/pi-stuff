34ee7a02168b70ca4721e73f8bc670693181fc7b
Peter Suggitt <me@suggs.org.uk>
2023-11-05 19:34:05 +0000

    added args for cfg file

--- speedtest/speedtest-influx.py
+++ speedtest/speedtest-influx.py
@@ -2,9 +2,17 @@ import re
 import subprocess
 import configparser
 from influxdb import InfluxDBClient
+import argparse
+
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-c", "--config", help="pass in a config file")
+args = argParser.parse_args()
+
+if args.config is None:
+    raise Exception("Need a config file please")
 
 cfg = configparser.ConfigParser()
-cfg.read('config.txt')
+cfg.read(args.config)
 
 response = subprocess.Popen('/usr/bin/speedtest --accept-license --accept-gdpr', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 
