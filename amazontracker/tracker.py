7d1b1ebd71bbe15d0ab67fd0047454686aeb537d
Peter Suggitt <me@suggs.org.uk>
2023-11-05 18:10:40 +0000

    tidy up

--- amazontracker/tracker.py
+++ amazontracker/tracker.py
@@ -7,19 +7,6 @@ HEADERS = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KH
 # HEADERS = {    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",    'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'}
 # HEADERS = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"}
 
-def configuration():
-    config = configparser.ConfigParser()
-    config.read('config.txt')
-    return config
-
-def create_db_client():
-    cfg = configuration()
-    return InfluxDBClient(host=cfg.get('influxdb', 'host'),
-                          port=cfg.get('influxdb', 'port'),
-                          username=cfg.get('influxdb', 'user'),
-                          password=cfg.get('influxdb', 'pass'),
-                          database='amazonprices')
-
 def get_prices():
     client = create_db_client()
     with open('trackurls.txt') as f:
@@ -58,6 +45,18 @@ def create_data(name, price):
             }
         }
     ]
+
+def create_db_client():
+    cfg = configuration()
+    return InfluxDBClient(host=cfg.get('influxdb', 'host'),
+                          port=cfg.get('influxdb', 'port'),
+                          username=cfg.get('influxdb', 'user'),
+                          password=cfg.get('influxdb', 'pass'),
+                          database='amazonprices')
+def configuration():
+    config = configparser.ConfigParser()
+    config.read('config.txt')
+    return config
 
 if __name__ == "__main__":
     get_prices()
